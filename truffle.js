0a87bdbff97f24b845f5410d62283fb4649fb2b6
Nicola Miotto <sirnicolaz@gmail.com>
2018-11-19 11:49:37 +0100

    fix config files

--- truffle.js
+++ truffle.js
@@ -1,18 +1,37 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('dotenv').config();
+
+const g = (name, fallback) => process.env[name] || fallback;
+
+const Web3 = require('web3');
+const web3 = new Web3();
+const WalletProvider = require('truffle-wallet-provider');
+const Wallet = require('ethereumjs-wallet');
+const myPrivateKey = g('PRIVATE_KEY', '')
+const node = g('NODE_HOST', 'http://localhost:7545')
+
+var ropstenProvider
+
+if (myPrivateKey) {
+  // read the private key `PRIVATE_KEY` from our ENV variables
+  let privateKey = new Buffer(myPrivateKey, 'hex')
+  // create a wallet using the private key
+  let wallet = Wallet.fromPrivateKey(privateKey);
+  // and initialize a wallet provider with a full node on infura.
+  provider = new WalletProvider(wallet, node);
+}
+
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      provider: provider,
+      // You can get the current gasLimit by running
+      // truffle deploy
+      // truffle(ropsten)> web3.eth.getBlock('pending', (error, result) =>
+      //   console.log(result.gasLimit))
+      gas: 4600000,
+      gasPrice: web3.utils.toWei('20', 'gwei'),
+      network_id: '*',
+    }
+  }
 };
