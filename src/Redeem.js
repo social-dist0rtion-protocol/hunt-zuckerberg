2685162ba3ceaba9428b7cdb835c9485b4b8c36c
Nicola Miotto <sirnicolaz@gmail.com>
2018-11-19 17:38:00 +0100

    bind redeem button to code state

--- src/Redeem.js
+++ src/Redeem.js
@@ -1,25 +1,46 @@
-import React, { Component } from "react";
-import { getWeb3, getContract } from "./util";
+import Web3Utils from 'web3-utils';
+import React, {Component} from 'react';
+import {getWeb3, getContract} from './util';
 
 class Redeem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   async handleRedeem() {
     const web3 = await getWeb3();
     const account = (await web3.eth.getAccounts())[0];
-    const contract = await getContract(web3, "HuntZuckerberg");
-    contract.methods
-      .redeem(this.props.match.params.code)
-      .send({ from: account });
+    const contract = await getContract(web3, 'HuntZuckerberg');
+    contract.methods.redeem(this.props.match.params.code).send({from: account});
+  }
+
+  async componentDidMount() {
+    const web3 = await getWeb3();
+    const contract = await getContract(web3, 'HuntZuckerberg');
+    const codeOwner = await contract.methods
+      .hashedCodeToPlayer(Web3Utils.keccak256(this.props.match.params.code))
+      .call();
+    this.setState({
+      isCodeAlreadyRedeemed:
+        codeOwner !== '0x0000000000000000000000000000000000000001',
+    });
   }
 
   render() {
     const code = this.props.match.params.code;
+    const {isCodeAlreadyRedeemed} = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <h1>Redeem</h1>
           <h2>{code}</h2>
-          <button onClick={this.handleRedeem.bind(this)}>Redeem</button>
+          <button
+            disabled={isCodeAlreadyRedeemed}
+            onClick={this.handleRedeem.bind(this)}>
+            Redeem
+          </button>
         </header>
       </div>
     );
