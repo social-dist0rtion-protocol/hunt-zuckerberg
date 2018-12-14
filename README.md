c290a3c439a1f66aa51ddc9c00bc808cc871acb8
vrde <agranzot@gmail.com>
2018-12-14 15:08:07 +0100

    add key gen via mnemonic

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# hunt-zuckerberg 
-Decentralized Treasure Hunt 
+# hunt-zuckerberg
+Decentralized Treasure Hunt
 
-# Run tests
+## Run DApp
+You need a network to deploy the contract for the game. You can use [Ganache][ganache], [Ganache CLI][ganache:cli], a (test) network through [infura][infura], or your own node.
+
+- deploy the contract with `truffle migrate`. If it fails you might try `truffle migrate --reset`.
+- `npm start`
+- open http://localhost:3000 on your wallet or on your browser (make sure you have metamask installed)
+
+## Run tests
 `./node_modules/.bin/mocha --reporter spec`
+
+
+[ganache]: https://truffleframework.com/ganache
+[ganache:cli]: https://github.com/trufflesuite/ganache-cli
+[infura]: https://infura.io/
--- truffle.js
+++ truffle.js
@@ -1,23 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const g = (name, fallback) => process.env[name] || fallback;
 
-const Web3 = require('web3');
+const Web3 = require("web3");
 const web3 = new Web3();
-const WalletProvider = require('truffle-wallet-provider');
-const Wallet = require('ethereumjs-wallet');
-const myPrivateKey = g('PRIVATE_KEY', '');
-const node = g('RPC_ENDPOINT', 'http://localhost:7545');
-
-var ropstenProvider;
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require("ethereumjs-wallet");
+const mnemonic = g("MNEMONIC", "test");
+const myPrivateKey = g("PRIVATE_KEY", "");
+const endpoint = g("RPC_ENDPOINT", "http://localhost:7545");
 
 if (myPrivateKey) {
   // read the private key `PRIVATE_KEY` from our ENV variables
-  let privateKey = new Buffer(myPrivateKey, 'hex');
+  let privateKey = new Buffer(myPrivateKey, "hex");
   // create a wallet using the private key
   let wallet = Wallet.fromPrivateKey(privateKey);
-  // and initialize a wallet provider with a full node on infura.
-  provider = new WalletProvider(wallet, node);
+  // and initialize a wallet provider with the selected endpoint
+  provider = new WalletProvider(wallet, endpoint);
+} else if (mnemonic) {
+  // and initialize a wallet provider with the selected endpoint
+  provider = new HDWalletProvider(mnemonic, endpoint);
 }
 
 module.exports = {
@@ -29,8 +32,8 @@ module.exports = {
       // truffle(ropsten)> web3.eth.getBlock('pending', (error, result) =>
       //   console.log(result.gasLimit))
       gas: 4600000,
-      gasPrice: web3.utils.toWei('20', 'gwei'),
-      network_id: '*',
-    },
-  },
+      gasPrice: web3.utils.toWei("20", "gwei"),
+      network_id: "*"
+    }
+  }
 };
