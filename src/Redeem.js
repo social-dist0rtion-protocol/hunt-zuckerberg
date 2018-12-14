5847a13e3db057c05e20241ec963bcfb75dd8dc7
vrde <agranzot@gmail.com>
2018-12-14 15:09:03 +0100

    lint

--- src/Redeem.js
+++ src/Redeem.js
@@ -1,6 +1,6 @@
-import Web3Utils from 'web3-utils';
-import React, {Component} from 'react';
-import {getWeb3, getContract} from './util';
+import Web3Utils from "web3-utils";
+import React, { Component } from "react";
+import { getWeb3, getWeb3Anon, getContract } from "./util";
 
 class Redeem extends Component {
   constructor(props) {
@@ -11,25 +11,28 @@ class Redeem extends Component {
   async handleRedeem() {
     const web3 = await getWeb3();
     const account = (await web3.eth.getAccounts())[0];
-    const contract = await getContract(web3, 'HuntZuckerberg');
-    contract.methods.redeem(this.props.match.params.code).send({from: account});
+    const contract = await getContract(web3, "HuntZuckerberg");
+    contract.methods
+      .redeem(this.props.match.params.code)
+      .send({ from: account });
   }
 
   async componentDidMount() {
     const web3 = await getWeb3();
-    const contract = await getContract(web3, 'HuntZuckerberg');
+    const contract = await getContract(web3, "HuntZuckerberg");
     const codeOwner = await contract.methods
       .hashedCodeToPlayer(Web3Utils.keccak256(this.props.match.params.code))
       .call();
+    console.log(codeOwner);
     this.setState({
       isCodeAlreadyRedeemed:
-        codeOwner !== '0x0000000000000000000000000000000000000001',
+        codeOwner !== "0x0000000000000000000000000000000000000001"
     });
   }
 
   render() {
     const code = this.props.match.params.code;
-    const {isCodeAlreadyRedeemed} = this.state;
+    const { isCodeAlreadyRedeemed } = this.state;
 
     return (
       <div className="App">
@@ -38,7 +41,8 @@ class Redeem extends Component {
           <h2>{code}</h2>
           <button
             disabled={isCodeAlreadyRedeemed}
-            onClick={this.handleRedeem.bind(this)}>
+            onClick={this.handleRedeem.bind(this)}
+          >
             Redeem
           </button>
         </header>
