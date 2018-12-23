6cfe2068090b31e05d28192f55ed14017da06cbb
Nicola Miotto <sirnicolaz@gmail.com>
2018-12-23 16:49:57 +0100

    fix a few things

--- setup/generate_tokens.js
+++ setup/generate_tokens.js
@@ -3,12 +3,10 @@ const web3 = require("web3");
 const seedrandom = require("seedrandom");
 const fs = require("fs");
 
-const SEED = process.env["SEED"];
-
-function generateTokens(count) {
+function generateTokens(seed, count) {
   var tokenMap = {};
   while (count-- > 0) {
-    const seedRnd = seedrandom(SEED + count);
+    const seedRnd = seedrandom(seed + count);
     const token = Buffer.from(seedRnd().toString()).toString("base64");
     const hashedToken = web3.utils.keccak256(token);
     tokenMap[token] = hashedToken;
@@ -19,7 +17,7 @@ function generateTokens(count) {
 
 const slicesFile = process.argv[2];
 const outFile = process.argv[3];
+const seedString = process.argv[4];
 const slices = JSON.parse(fs.readFileSync(slicesFile, "utf8"));
-
-const tokenMap = generateTokens(slices.length);
+const tokenMap = generateTokens(seedString, slices.length);
 fs.writeFileSync(outFile, JSON.stringify(tokenMap, null, 2), function(err) {});
