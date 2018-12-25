9818295d3fe592971cc0fa95785b11a19ea658a3
vrde <agranzot@gmail.com>
2018-12-25 22:23:22 +0100

    manage entropy for the PRNG

--- setup/generate_tokens.js
+++ setup/generate_tokens.js
@@ -3,23 +3,26 @@ const web3 = require("web3");
 const seedrandom = require("seedrandom");
 const fs = require("fs");
 
+function generateToken(prng, bits, chars) {
+  if (chars === undefined) {
+    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  }
+  let token = "";
+  for (let i = 0; i < Math.ceil(bits / Math.log2(chars.length)); i++) {
+    token += chars[Math.floor(prng() * chars.length)];
+  }
+  return token;
+}
+
 function generateTokens(seed, count) {
   var tokenMap = {};
   const rnd = seedrandom(seed);
   while (count-- > 0) {
     var source = "";
     var parts = [];
-    // each call of rnd generates 32 bits of randomness in a float,
-    // call it 4 times and you get 128 bits
+    // Each call of rnd generates 32 bits of randomness in a float.
     for (var i = 0; i < 4; i++) {
-      source += rnd().toString();
-    }
-    // get the hash of the source string, remove "0x"
-    const image = web3.utils.keccak256(source).substr(2);
-
-    // split the string in four 8-char parts
-    for (var i = 0; i < 4; i++) {
-      parts.push(image.substr(i * 8, 8));
+      parts.push(web3.utils.keccak256(rnd().toString()).substr(2, 8));
     }
     const token = parts.join("-");
     const hashedToken = web3.utils.keccak256(token);
