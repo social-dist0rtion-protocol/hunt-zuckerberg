30eaab85b5eb18b7fd8ac0a880328e97ed3ce4da
vrde <agranzot@gmail.com>
2018-12-26 21:51:29 +0100

    update styling

--- setup/generate_qrcodes.js
+++ setup/generate_qrcodes.js
@@ -2,11 +2,23 @@ const QRCode = require("qrcode");
 
 const tokens = require(process.argv[2]);
 const { homepage } = require("../package.json");
+const SIZE = 180;
+
+const titles = [
+  "All your Mark Zuckerbergs are belong to us.",
+  "A wild Mark Zuckerberg appears",
+  "Refreshing memories of Mark Zuckerberg",
+  "Welcome to 2055. This is a piece of Mark Zuckerberg",
+  "Cut my Mark into pieces, this is my last resort",
+  "640k of Mark should be enough for anybody",
+  "206 Partial Mark Found"
+];
 
 function renderQRCode(qrcode) {
+  const title = titles[Math.floor(Math.random() * titles.length)];
   return `
   <div>
-  hunt zuckerberg
+  ${title}
   <img src="${qrcode}"/>
   </div>
   `;
@@ -16,7 +28,9 @@ async function renderQRCodes(tokens) {
   const keys = Object.keys(tokens);
   const qrcodes = [];
   for (var i = 0; i < keys.length; i++) {
-    qrcodes.push(await QRCode.toDataURL(`${homepage}/redeem/${keys[i]}`));
+    qrcodes.push(
+      await QRCode.toDataURL(`https://huntzuckerberg.xyz/#/redeem/${keys[i]}`)
+    );
   }
 
   return `
@@ -24,8 +38,23 @@ async function renderQRCodes(tokens) {
 <html lang="en">
   <head>
   <style>
-  div { width: 200px; border: 2px solid black; margin: 10px; text-align: center;
-  display: inline-block; }
+  div {
+  font-family: monospace;
+    font-size: ${SIZE / 10}px;
+    width: ${SIZE}px;
+    border: 2px solid black;
+    margin: 10px;
+    text-align: center;
+    display: inline-block;
+    padding: 10px
+  }
+  img {
+    width: ${SIZE}px;
+    height: ${SIZE}px;
+    image-rendering: pixelated;
+    display: block;
+    margin: 0 auto;
+  }
   </style>
   </head>
   <body>
