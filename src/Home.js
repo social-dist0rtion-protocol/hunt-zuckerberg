89d07ba8faaca77c33e7a0c96a1d9c4fe25c4396
Nicola Miotto <sirnicolaz@gmail.com>
2018-12-27 16:50:49 +0100

    add missing Home.js

---
+++ src/Home.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Refreshing memories of "MARK ZUCKERBERG"</h1>
+        <p>
+          It's the year 2055.
+          <br />
+          MARK ZUCKERBERG uploaded itself in the Ethereum Network because
+          blockchain and stuff and user data and Ads.
+          <br />
+          HELP US finding The MARK pieces and stop it!
+          <br />
+        </p>
+        <p>
+          Each MARK piece is a token. You can find them by searching QR codes
+          about MARK at the CCC. You will recognize them as soon as you find
+          them.
+          <br />
+          Each token can be redeemed with and Ethereum transaction (so you will
+          need wallet enabled browser and some Ethereum on Rinkeby).
+          <br />
+          Once you find a piece, you can come to the Social Dist0rtion Protocol
+          assembly and redeem a prize. The winner will get a YUBI key!!!111 one
+        </p>
+        <h1>Requirements</h1>
+        <ul>
+          <li>
+            A wallet enabled browser or extension: Metamask on desktop, Cipher
+            on mobile
+          </li>
+          <li>
+            An Ethereum wallet with Eth (on Rinkeby, so no real money involved)
+          </li>
+        </ul>
+        <h1>Support</h1>
+        <ul>
+          <li>
+            Come to the Social Dist0rtion Protocol assembly if you want to talk
+            to <b>real people</b>
+          </li>
+          <li>
+            DECT numbers:
+            <ul>
+              <li>vrde: 6665</li>
+              <li>sirnicolaz: 6616</li>
+            </ul>
+          </li>
+        </ul>
+        <h1>Prizes</h1>
+        <ul>
+          <li>
+            For each unlocked MARK piece:
+            <ul>
+              <li>Grappa</li>
+              <li>Berliner Luft</li>
+              <li>Maybe other stuff? :j</li>
+            </ul>
+          </li>
+          <li>
+            For the winner (most pieces collected): a brand new <b>Yubi Key</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
