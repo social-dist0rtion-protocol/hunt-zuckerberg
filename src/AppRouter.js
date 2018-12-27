dd1bc4e17ff44a7b70a661125be59136f6bec993
Nicola Miotto <sirnicolaz@gmail.com>
2018-12-27 19:04:51 +0100

    remove About, add Home content (#15)
    * remove About, add Home content
    
    * add missing Home.js
    
    * some improvements
    
    * better titles

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,10 @@
 import React from "react";
-import About from "./About";
+import Home from "./Home";
 import Redeem from "./Redeem";
 import NotFound from "./NotFound";
 import Visualize from "./Visualize";
 import LeaderBoard from "./LeaderBoard";
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
-
-const Index = () => <h2>Home</h2>;
 
 const AppRouter = () => (
   <Router>
@@ -17,13 +15,10 @@ const AppRouter = () => (
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">The Game</Link>
             </li>
             <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/visualize/">Visualize</Link>
+              <Link to="/visualize/">Current State</Link>
             </li>
             <li>
               <Link to="/leader-board/">Leader Board</Link>
@@ -34,9 +29,8 @@ const AppRouter = () => (
 
       <main>
         <Switch>
-          <Route path="/" exact component={Index} />
+          <Route path="/" exact component={Home} />
           <Route path="/redeem/:token" component={Redeem} />
-          <Route path="/about/" component={About} />
           <Route path="/visualize/" component={Visualize} />
           <Route path="/leader-board/" component={LeaderBoard} />
           <Route component={NotFound} />
---
+++ src/Home.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Refreshing memories of "MARK ZUCKERBERG"</h1>
+        <p>
+          It's the year 2055.
+          <br />
+          MARK ZUCKERBERG uploaded itself in the Ethereum Network because
+          blockchain and stuff and user data and Ads.
+          <br />
+          HELP US finding The MARK pieces and stop it!
+          <br />
+        </p>
+        <p>
+          Each piece of Mark (a ZUCKERBIT) is a token. You can find them by
+          searching QR codes about MARK at the CCC. You will recognize them as
+          soon as you find them.
+          <br />
+          Each token can be redeemed with and Ethereum transaction (so you will
+          need wallet enabled browser and some Ether on Rinkeby).
+          <br />
+          Once you find a ZUCKERBIT, you can come to the Social Dist0rtion
+          Protocol assembly and redeem a prize. The winner will get a YUBI
+          key!!!111 one
+        </p>
+        <h1>Requirements</h1>
+        <ul>
+          <li>
+            A wallet enabled browser or extension: Metamask on desktop, Cipher
+            on mobile
+          </li>
+          <li>
+            An Ethereum wallet with Ether on the Rinkeby test network, so no
+            real money involved. We can also send you some if you need.
+          </li>
+        </ul>
+        <h1>Support</h1>
+        <ul>
+          <li>
+            Come to the{" "}
+            <a href="https://35c3.c3nav.de/l/social-dist0rtion-protocol/">
+              Social Dist0rtion Protocol assembly
+            </a>{" "}
+            if you want to talk to <b>real people</b>.
+          </li>
+          <li>
+            DECT numbers:
+            <ul>
+              <li>vrde: 6665</li>
+              <li>sirnicolaz: 6616</li>
+            </ul>
+          </li>
+        </ul>
+        <h1>Prizes</h1>
+        <ul>
+          <li>
+            For each unlocked MARK piece:
+            <ul>
+              <li>Grappa</li>
+              <li>Berliner Luft</li>
+              <li>Maybe other stuff? :j</li>
+            </ul>
+          </li>
+          <li>
+            For the winner (most ZUCKERBITS collected): a brand new{" "}
+            <b>Yubi Key</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
